51c5163168d698c33b9aba5079d4d0a3b9809d02
David J Mosher <davidjosemosher@gmail.com>
2019-09-25 08:48:52 -0700

    Add initial files from local

---
+++ Tic-Tac-Toe/script.js
@@ -0,0 +1,10 @@
+// $('.square').on('click', function(){
+//   $('.square') = 'x';
+// });
+
+var $elements = document.getElementsByClassName('square');
+for(var i = 0; i < $elements.length; i++){
+  element.on('click', function(){
+    element.text = 'x';
+  });
+};
