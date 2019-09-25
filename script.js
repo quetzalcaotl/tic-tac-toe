c2882bed00385a490f358659c55499d9716fc879
David José Mosher <davidjosemosher@gmail.com>
2019-09-25 09:07:44 -0700

    Correct eventListener function invocation and     location of script tag in HTML file

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// $('.square').on('click', function(){
-//   $('.square') = 'x';
-// });
-
 var $elements = document.getElementsByClassName('square');
 for(var i = 0; i < $elements.length; i++){
-  element.on('click', function(){
-    element.text = 'x';
+  $elements[i].addEventListener('click', function(e){
+    e.target.text = 'x';
   });
 };
